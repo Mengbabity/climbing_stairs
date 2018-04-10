baf02686d5d4aa6eb4d93646622ac57e342d5f00
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2018-04-10 16:39:37 +0800

    Update climb.cpp

--- climb.cpp
+++ climb.cpp
@@ -1,4 +1,4 @@
-class Solution {
+   class Solution {
 public:
     int climbStairs(int n) {
         if(n<=0)
@@ -15,8 +15,8 @@ public:
         for(int i=3;i<=n;i++)
         {
             count=one+two;
-            two=one;
-            one=count;
+            two=one;   //两步中当前的第二步为之前第一步
+            one=count;   //两步中当前第一步为上一次的top
         }
         
         return count;
